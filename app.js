7efbe28c548764deb3017feba82e6216efb6cdd2
VivekAlhat <vivekalhat14@outlook.com>
2020-08-08 18:37:57 +0530

    Signup design added

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const https = require("https");
 // Create server
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Start server
 app.listen(3000, function () {
   console.log("Server is up and running on port 3000");
+});
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/src/signup.html");
 });
