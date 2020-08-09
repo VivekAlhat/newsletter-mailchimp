6ecce38e36453f22c5aab6fc41277e5386ec84df
VivekAlhat <vivekalhat14@outlook.com>
2020-08-09 17:39:09 +0530

    Added method to post data to mailchimp

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # newsletter-mailchimp
+
 Email-newsletter website using Node and Mailchimp
+
+## How to use ?
+
+- Clone the repository and generate .env file
+- Sign up for a Mailchimp account.
+- Fill in your private API key, List ID & Server Code as per the variables\ used in `app.js`.
+- Use `nodemon --exec npm start` or `node app.js` to build the project.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+require("dotenv").config();
 
 // Create server
 const app = express();
@@ -16,6 +17,50 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/src/signup.html");
 });
 
+// Post data to mailchimp
 app.post("/", function (request, response) {
   const userEmail = request.body.email;
+  const data = {
+    members: [
+      {
+        email_address: userEmail,
+        status: "subscribed",
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  //  API auth details
+  const serverCode = process.env.SERVERCODE;
+  const listID = process.env.LISTID;
+  const apiKey = process.env.APIKEY;
+
+  const url =
+    "https://" + serverCode + ".api.mailchimp.com/3.0/lists/" + listID;
+  const options = {
+    method: "POST",
+    auth: "apiKey:" + apiKey,
+  };
+  const req = https.request(url, options, function (res) {
+    res.on("data", function (data) {
+      const r = JSON.parse(data);
+      const errCount = r.error_count;
+      if (errCount > 0) {
+        response.sendFile(__dirname + "/src/error.html");
+      } else {
+        response.sendFile(__dirname + "/src/success.html");
+      }
+    });
+  });
+  req.write(jsonData);
+  req.end();
+});
+
+app.post("/success", function (req, res) {
+  res.redirect("/");
+});
+
+app.post("/error", function (req, res) {
+  res.redirect("/");
 });
